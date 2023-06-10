962355fa6f0874e57d6b97f2353b1d97e37c8dd7
Ghernandez1991 <hernandez.gabriel59@yahoo.com>
2023-06-09 22:23:25 -0500

    added fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
 import pytest
+import pandas as pd
 
 @pytest.fixture
 def city_list():
     return ["London", "Paris", "New York"]
+
+
+
+@pytest.fixture
+def dataframe():
+    one_df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+    two_df = pd.DataFrame({'A': [7, 8, 9], 'B': [10, 11, 12]})
+    return one_df,two_df
