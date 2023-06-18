6276ca8924ba08a5da06d42c346d8e6f099fed43
Ghernandez1991 <hernandez.gabriel59@yahoo.com>
2023-06-18 16:21:09 -0500

    functionality to write to sqlite

--- write_to_processed.py
+++ write_to_processed.py
@@ -5,10 +5,19 @@ from pathlib import Path
 def main():
     data_manipulation_class = DataManipulation()
     validated_path = Path(f"validated_data/")
-    df = data_manipulation_class.concat_all_frames(validated_path)
+    try:
+        df = data_manipulation_class.concat_all_frames(validated_path)
+    except ValueError:
+        print("no files to concat")
+        pass
     for file in validated_path.glob("*"):
         print(file.parts[-1])
         data_manipulation_class.move_to_processed_from_validated(file.parts[-1])
+
+    db_path = Path("weather.db")
+    csv_path = Path("processed_data/all_data.csv")
+    table_name = "all_weather_data"
+    data_manipulation_class.write_csv_to_sqlite(csv_path, db_path, table_name)
 
 
 if __name__ == "__main__":
